0cc90b520b9fa6e24dd2fa8cdc649f14769b21c1
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-02-23 20:17:34 -0300

    changing login order

--- src/routes/aut/ProfilePage.js
+++ src/routes/aut/ProfilePage.js
@@ -11,7 +11,7 @@ export const Profile = () => {
     return (
         isAuthenticated && (
             <div>
-            <img src={user.picture} alt={user.name} />
+            {/* <img src={user.picture} alt={user.name} /> */}
             <h2>{user.name}</h2>
             <p>Email:{user.email}</p>
             </div>
