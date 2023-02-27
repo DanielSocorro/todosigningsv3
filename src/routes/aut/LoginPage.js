e27999529f8b4bfaebc75614280672b94b71b5a4
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-02-27 20:40:54 -0300

    styling login4

--- src/routes/aut/LoginPage.js
+++ src/routes/aut/LoginPage.js
@@ -1,16 +1,32 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useAuth0 } from "@auth0/auth0-react";
 
 function LoginButton() {
   const { loginWithRedirect } = useAuth0();
-  return <Button onClick={() => loginWithRedirect()}>Manchester United Signing's App</Button>
+  return (
+    <Button 
+      sx={{
+        background: 'white',
+        color: 'black',
+        fontSize: '2.4rem',
+        borderRadius: '60px',
+        float: 'right',
+        fontWeight:'bolder',
+        marginRight: '100px',
+        marginTop: '53rem',
+        textTransform: 'inherit',
+        padding: '36px',
+        height: '100px',
+      }}             
+      onClick={() => loginWithRedirect()}
+    >
+      Login with Gmail
+    </Button>
+  );
 }
 
 export function SignInSide() {
@@ -21,7 +37,6 @@ export function SignInSide() {
         <Grid
           item
           xs={12}
-          md={7}
           sx={{
             position: 'relative',
             height: '100vh',
@@ -37,26 +52,50 @@ export function SignInSide() {
             style={{
               position: 'absolute',
               top: 0,
-              right: 0,
-              width: '50%',
+              left: '60%',
+              width: '40%',
               height: '100%',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              backgroundColor: 'rgba(0, 0, 0, 0.75)',
+              backdropFilter: 'blur(10px)',
             }}
           />
-        </Grid>
-        <Grid item xs={12} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+          <img
+            src="https://i.ibb.co/prrxrDr/manchester-united-fc-logo.png"
+            alt="Manchester United logo"
+            style={{
+              position: 'absolute',
+              top: '-13%',
+              right: '1%',
+              width: '9%',
+              height: '12%',
+            }}
+          />
+          <img
+            src="https://i.ibb.co/Wzndr52/android-chrome-512x512.png"
+            alt="Android Chrome logo"
+            style={{
+              position: 'absolute',
+              top: '2%',
+              left: '20%',
+              transform: 'translateX(-50%)',
+              width: '14%',
+              height: '18%',
+            }}
+          />
+          <h1
+            style={{
+              position: 'absolute',
+              top: '31%',
+              left: '82%',
+              transform: 'translateX(-50%)',
+              textAlign: 'center',
+              color: 'white',
+              fontSize: '2.1rem',
             }}
           >
-          {/*   <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} /> */}
-            <LoginButton />
-          </Box>
+            Manchester United Signing's app
+          </h1>
+          <LoginButton />
         </Grid>
       </Grid>
     </ThemeProvider>
