fb7ab8b2ac4204426e4ff10fe47a9d5e5f619581
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-03-02 18:13:39 -0300

    fixing homepge ux

--- src/routes/aut/LogoutPage.js
+++ src/routes/aut/LogoutPage.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Button from '@mui/material/Button';
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 
 export const LogoutButton = () => {
     const { logout } = useAuth0();
 
-    return <Button variant="contained" onClick={() => logout({
+    return <button 
+    className="logoutbtn"
+     onClick={() => logout({
          returnTo: window.location.origin
-        })}>Logout</Button>
+        })}>Logout</button>
 }
--- src/routes/aut/ProfilePage.js
+++ src/routes/aut/ProfilePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./ProfilePage.css";
 
 export const Profile = () => {
     const { user, isAuthenticated, isLoading} = useAuth0();
@@ -10,10 +11,20 @@ export const Profile = () => {
 
     return (
         isAuthenticated && (
-            <div>
-            <img src={user.picture} alt={user.name} />
-            <h2>Bienvenido:   {user.name}</h2>
-           {/*  <p>Email:{user.email}</p> */}
+            <div 
+            className="container">
+            <img
+            className="logoman"
+            src="https://i.ibb.co/qLSjFdv/mu-logo.png"
+            alt="logomu"
+             />
+            <img 
+            typeof="png"
+            className="userPicture"
+             src={user.picture} alt={user.name} />
+            <h2
+            className="greeting"
+            >Welcome <p>{user.name}</p></h2>
             </div>
         )
     );
