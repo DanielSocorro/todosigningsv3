4a5eeedcfa28265f9de88eaa9352c7531629664c
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-04-06 17:43:16 -0400

    fixing mobile resolution

--- src/routes/aut/LoginPage.js
+++ src/routes/aut/LoginPage.js
@@ -40,8 +40,7 @@ export function SignInSide() {
         width: '100vw',
         backgroundImage: 'url(https://i.ibb.co/WBYMGdX/manuntd.jpg)',
         backgroundRepeat: 'no-repeat',
-        backgroundColor: (t) =>
-          t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+     
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
